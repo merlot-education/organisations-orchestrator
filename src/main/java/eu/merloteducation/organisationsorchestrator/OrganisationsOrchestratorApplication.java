8456b113f4daf8247aa1bbc4245d604963e63566
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2023-04-13 16:19:25 +0200

    Initial checkin

---
+++ src/main/java/eu/merloteducation/organisationsorchestrator/OrganisationsOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package eu.merloteducation.organisationsorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisationsOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganisationsOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/merloteducation/organisationsorchestrator/OrganisationsOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.merloteducation.organisationsorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisationsOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

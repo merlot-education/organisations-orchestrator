e62f1bef959b3081c0969c5bbe4b38bf35b1bf88
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-15 14:47:18 +0200

    added public view to address fields

--- src/main/java/eu/merloteducation/organisationsorchestrator/models/AddressModel.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/models/AddressModel.java
@@ -1,13 +1,19 @@
 package eu.merloteducation.organisationsorchestrator.models;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 
 @Getter
 public class AddressModel {
+    @JsonView(OrganiationViews.PublicView.class)
     private String countryCode;
+    @JsonView(OrganiationViews.PublicView.class)
     private String postalCode;
+    @JsonView(OrganiationViews.PublicView.class)
     private String addressCode;
+    @JsonView(OrganiationViews.PublicView.class)
     private String city;
+    @JsonView(OrganiationViews.PublicView.class)
     private String street;
 
     public AddressModel(VCard vcard, String addressCode) {

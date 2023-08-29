d695382c9540cc40132a3fcb862c1eb71acc913a
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-08-29 13:25:04 +0200

    remove import

--- src/main/java/eu/merloteducation/organisationsorchestrator/controller/OrganisationConnectorsController.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/controller/OrganisationConnectorsController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;

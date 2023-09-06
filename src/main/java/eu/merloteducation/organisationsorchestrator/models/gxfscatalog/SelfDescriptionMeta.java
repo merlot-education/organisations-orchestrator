5013c5676dcd9b0eff3d8fe51c9a0fa69506985c
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-09-06 13:56:02 +0200

    remove unused import

--- src/main/java/eu/merloteducation/organisationsorchestrator/models/gxfscatalog/SelfDescriptionMeta.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/models/gxfscatalog/SelfDescriptionMeta.java
@@ -1,6 +1,5 @@
 package eu.merloteducation.organisationsorchestrator.models.gxfscatalog;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 

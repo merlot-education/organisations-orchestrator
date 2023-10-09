effede2e874dcec8e1dd3acb3627fc0b47b9736e
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2023-10-09 15:20:37 +0200

    Minor adjustment.

--- src/main/java/eu/merloteducation/organisationsorchestrator/controller/OrganizationQueryController.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/controller/OrganizationQueryController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;

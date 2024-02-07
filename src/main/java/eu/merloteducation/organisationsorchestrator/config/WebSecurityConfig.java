2f3d7d5810eb47c44672fc69bf3dba9019a06858
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-02-07 15:37:58 +0100

    first tests with removing keycloak for ssi

--- src/main/java/eu/merloteducation/organisationsorchestrator/config/WebSecurityConfig.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/config/WebSecurityConfig.java
@@ -24,9 +24,10 @@ public class WebSecurityConfig {
 
         http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/health")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
-            .requestMatchers(new AntPathRequestMatcher("/federators")).permitAll()
+            //.requestMatchers(new AntPathRequestMatcher("/federators")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/organization/*")).permitAll().anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
+        http.oauth2ResourceServer().jwt();//.jwtAuthenticationConverter(jwtAuthConverter);
+        //http.oauth2Login();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors();
         http.headers().frameOptions().disable();

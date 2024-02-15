9f27cd20fd483b4406edf260d7a562d7159980d5
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-02-15 14:22:11 +0100

    clean up remainders of auth

--- src/main/java/eu/merloteducation/organisationsorchestrator/config/WebSecurityConfig.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/config/WebSecurityConfig.java
@@ -1,37 +1,32 @@
 package eu.merloteducation.organisationsorchestrator.config;
 
-import eu.merloteducation.authorizationlibrary.authorization.JwtAuthConverter;
-import lombok.RequiredArgsConstructor;
+import eu.merloteducation.authorizationlibrary.config.MerlotSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
 
-    private final JwtAuthConverter jwtAuthConverter;
+    @Autowired
+    private MerlotSecurityConfig merlotSecurityConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/health")).permitAll()
-            .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
-            //.requestMatchers(new AntPathRequestMatcher("/federators")).permitAll()
-            .requestMatchers(new AntPathRequestMatcher("/organization/*")).permitAll()
-                .anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
-        //http.oauth2Login();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors();
-        http.headers().frameOptions().disable();
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers(new AntPathRequestMatcher("/health")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/federators")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/organization/*")).permitAll()
+                .anyRequest().authenticated());
+        merlotSecurityConfig.applySecurityConfig(http);
         return http.build();
     }
 }

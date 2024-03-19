e8cce37bc9a6924c8a13bb73b69eb68ae27c15fe
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-03-19 13:57:47 +0100

    initialize cipher on object creation

--- src/main/java/eu/merloteducation/organisationsorchestrator/models/AttributeEncryptor.java
+++ src/main/java/eu/merloteducation/organisationsorchestrator/models/AttributeEncryptor.java
@@ -18,21 +18,24 @@ import java.util.Base64;
 public class AttributeEncryptor implements AttributeConverter<String, String> {
 
     private final Key key;
-    private final Cipher cipher;
+    private final Cipher cipherEncrypt;
+    private final Cipher cipherDecrypt;
 
     public AttributeEncryptor(@Value("${db.encryption.key}") String encryptionKey,
                               @Value("${db.encryption.transformation:AES}") String transformation) // TODO check options
-            throws NoSuchPaddingException, NoSuchAlgorithmException {
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         key = new SecretKeySpec(encryptionKey.getBytes(), transformation);
-        cipher = Cipher.getInstance(transformation);
+        cipherEncrypt = Cipher.getInstance(transformation);
+        cipherEncrypt.init(Cipher.ENCRYPT_MODE, key);
+        cipherDecrypt = Cipher.getInstance(transformation);
+        cipherDecrypt.init(Cipher.DECRYPT_MODE, key);
     }
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
         try {
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
-        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            return Base64.getEncoder().encodeToString(cipherEncrypt.doFinal(attribute.getBytes()));
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
             throw new IllegalStateException(e);
         }
     }
@@ -40,9 +43,8 @@ public class AttributeEncryptor implements AttributeConverter<String, String> {
     @Override
     public String convertToEntityAttribute(String dbData) {
         try {
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
-        } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            return new String(cipherDecrypt.doFinal(Base64.getDecoder().decode(dbData)));
+        } catch (BadPaddingException | IllegalBlockSizeException e) {
             throw new IllegalStateException(e);
         }
     }
